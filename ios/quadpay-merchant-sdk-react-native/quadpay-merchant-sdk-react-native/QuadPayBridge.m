4d4af29783fd475a24968820a69e194e1b094c23
Amanda Flink <avflinkette@gmail.com>
2021-01-27 14:33:32 -0500

    mfpp support

--- ios/quadpay-merchant-sdk-react-native/quadpay-merchant-sdk-react-native/QuadPayBridge.m
+++ ios/quadpay-merchant-sdk-react-native/quadpay-merchant-sdk-react-native/QuadPayBridge.m
@@ -124,6 +124,8 @@ RCT_EXPORT_METHOD(startVirtualCheckout:
                   customerCity:(NSString*) customerCity
                   customerState:(NSString*) customerState
                   customerCountry:(NSString*) customerCountry
+                  merchantFeeForPaymentPlan:(NSString*) merchantFeeForPaymentPlan
+
     ) {
     dispatch_async(dispatch_get_main_queue(), ^{
       QuadPayCheckoutDetails* details = [QuadPayCheckoutDetails alloc];
@@ -139,6 +141,7 @@ RCT_EXPORT_METHOD(startVirtualCheckout:
         details.customerFirstName = customerFirstName;
         details.customerLastName = customerLastName;
         details.customerEmail = customerEmail;
+        details.merchantFeeForPaymentPlan = merchantFeeForPaymentPlan;
 
       QuadPayVirtualCheckoutViewController* view = [QuadPayVirtualCheckoutViewController startCheckout:self details:details];
           UIViewController *rootViewController = RCTPresentedViewController();
@@ -160,6 +163,7 @@ RCT_EXPORT_METHOD(startCheckout:
                   customerCity:(NSString*) customerCity
                   customerState:(NSString*) customerState
                   customerCountry:(NSString*) customerCountry
+                  merchantFeeForPaymentPlan:(NSString*) merchantFeeForPaymentPlan
     ) {
     dispatch_async(dispatch_get_main_queue(), ^{
       QuadPayCheckoutDetails* details = [QuadPayCheckoutDetails alloc];
@@ -175,6 +179,7 @@ RCT_EXPORT_METHOD(startCheckout:
           details.customerFirstName = customerFirstName;
           details.customerLastName = customerLastName;
           details.customerEmail = customerEmail;
+          details.merchantFeeForPaymentPlan = merchantFeeForPaymentPlan;
 
       QuadPayCheckoutViewController* view = [QuadPayCheckoutViewController startCheckout:self details:details];
           UIViewController *rootViewController = RCTPresentedViewController();
