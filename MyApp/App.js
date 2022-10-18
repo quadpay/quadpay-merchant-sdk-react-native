5029d4d670dd1fd12afefaec1c9e571154eeb3bf
Petros Andreou <Petros.andreou@zip.co>
2022-10-18 14:55:19 +0100

    Add the default rn app

---
+++ MyApp/App.js
@@ -0,0 +1,87 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the UI Kitten template
+ * https://github.com/akveo/react-native-ui-kitten
+ *
+ * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
+ *
+ * @format
+ */
+
+import React, {useState, useEffect} from 'react';
+import { StyleSheet } from 'react-native';
+import {
+  ApplicationProvider,
+  IconRegistry,
+  Layout,
+} from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import * as eva from '@eva-design/eva';
+import Dropdown from './components/Dropdown/Dropdown';
+import Checkout from './components/Checkout/Checkout';
+import VirtualCheckout from './components/VirtualCheckout/VirtualCheckout';
+
+/**
+ * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
+ * https://akveo.github.io/eva-icons
+ */
+
+
+export default function App(){
+
+ let environments = [
+   'CI - use sandbox since no merchant configured for ci yet',
+    'Sandbox'
+  ];
+
+  let locales= [
+    'US',
+    'MX'
+  ]
+
+const [environment, setSelectedEnvironment] = useState(undefined);
+const [locale,setSelectedLocale] = useState(undefined)
+
+return(
+  <>
+    <IconRegistry icons={EvaIconsPack}/>
+    <ApplicationProvider {...eva} theme={eva.light}>
+    
+      <Layout style={styles.container}>
+      <Dropdown data= {environments} onSelect={setSelectedEnvironment}/>
+      <Dropdown data= {locales} onSelect={setSelectedLocale}/>
+      <Checkout locale={locale} environment={environment} merchantId ="5898b9a9-46bb-4647-92ed-52643d019d8c"/>
+      <VirtualCheckout locale={locale} environment={environment} merchantId ="5898b9a9-46bb-4647-92ed-52643d019d8c"/>
+      </Layout>
+    </ApplicationProvider>
+  </>
+);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonsContainer:{
+    flexDirection: 'row',
+    alignContent: 'space-around',
+    flexWrap: 'wrap'
+  },
+  button: {
+    backgroundColor: "blue",
+    padding:10,
+    borderRadius: 40,
+    marginBottom:5,
+    alignItems:'center'
+  },
+  likeButton: {
+    marginVertical: 16,
+  },
+});
+
+
+
---
+++ MyApp/components/Checkout/Checkout.js
@@ -0,0 +1,49 @@
+import React,{useEffect} from 'react';
+import { StyleSheet } from 'react-native';
+import { Button } from '@ui-kitten/components';
+import {QuadPay} from "quadpay-merchant-sdk-react-native";
+
+const Checkout =(props)=>{
+    const {
+        environment,
+        locale,
+        merchantId
+    } = props;
+
+    const makeAnOrder = async() =>{
+
+        let order = {
+          amount: "44.44",
+          merchantReference: "any random order",
+          customerFirstName: "Quincy",
+          customerEmail: "testing_12339283@test.com",
+          customerLastName: "Payman",
+          customerPhoneNumber: "+18146225937",
+          customerAddressLine1: "240 Meeker Ave",
+          customerAddressLine2: "Apt 35",
+          customerPostalCode: "11211",
+          customerCity: "Brooklyn",
+          customerState: "NY",
+          customerCountry: "US",
+          merchantFeeForPaymentPlan: "0",
+        };
+        QuadPay.initialize(merchantId,environment,locale)
+      
+        QuadPay.startCheckout(order);
+      
+      }
+
+    return(
+        <Button style={styles.likeButton} onPress={makeAnOrder}>
+            Standard order
+        </Button>
+    );
+}
+
+const styles = StyleSheet.create({
+    likeButton: {
+      marginVertical: 16,
+    },
+  });
+
+export default Checkout
